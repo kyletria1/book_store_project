ef52c50f248d25afadc8a2c3fd8e89b2afb014d0
kyletria1 <kyleadriantria01@gmail.com>
2025-09-14 08:49:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Excerpt from './Excerpt.jsx';
+
+function App(){
+  return (
+    <div>
+        <Excerpt title="React is awsome!" body="lorem ipsum..."/>
+        <Excerpt title="React is a JS library" body="It uses the shadow DOM"/>
+        <Excerpt title="Bla bla bla" body="bla bla bla"/>
+    </div>
+  );
+}
+
+
+
+export default App;
