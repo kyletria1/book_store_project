fd66b2007c66e38c1f5a0bc6455d33ea2bae9fda
kyletria1 <kyleadriantria01@gmail.com>
2025-09-14 16:33:36 -0700

    make file edits

--- book_store_project/src/App.jsx
+++ book_store_project/src/App.jsx
@@ -1,12 +1,34 @@
 import Book from './Book.jsx';
+import './index.css'; // ensures styles are applied
 
-function App(){
+const books = [
+  {
+    image: "https://itbook.store/img/books/9781484287507.png",
+    title: "Programming for Absolute Beginners",
+    authors: "Jonathan Bartlett",
+    link: "https://itbook.store/books/9781484287507"
+  },
+  {
+    image: "https://itbook.store/img/books/9781484292464.png",
+    title: "Build Your Own Test Framework",
+    authors: "Daniel Irvine",
+    link: "https://itbook.store/books/9781484292464"
+  }
+];
+
+export default function App() {
   return (
-    <div>
-        <Book image="https://itbook.store/img/books/9781484287507.png" title="Programming for Absolute Beginners" authors="Jonathan Bartlett" link="https://itbook.store/books/9781484287507"/>
-        <Book image="https://itbook.store/img/books/9781484292464.png" title="Build Your Own Test Framework" authors="Daniel Irvine" link="https://itbook.store/books/9781484292464"/>
+    <div className="book_container">
+      <div className="add_book">Add Book</div>
+      {books.map((book, index) => (
+        <Book
+          key={index}
+          image={book.image}
+          title={book.title}
+          authors={book.authors}
+          link={book.link}
+        />
+      ))}
     </div>
   );
 }
-
-export default App;
