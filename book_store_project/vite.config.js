cb9fc4a28b30ed4f4ab45ccfa7d5b79aff9855e4
kyletria1 <kyleadriantria01@gmail.com>
2025-09-14 15:41:32 -0700

    structure changes

--- book_store_project/vite.config.js
+++ book_store_project/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: 'dist',
     emptyOutDir: true
   }
 });
